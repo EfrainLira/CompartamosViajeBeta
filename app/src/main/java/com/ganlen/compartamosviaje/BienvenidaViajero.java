9eaf4988bc903908b9ee22e3f68138ee4d7e61dd
EfrainLira <efrawero@gmail.com>
2018-02-06 17:54:28 -0600

    6-02-17

---
+++ app/src/main/java/com/ganlen/compartamosviaje/BienvenidaViajero.java
@@ -0,0 +1,13 @@
+package com.ganlen.compartamosviaje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BienvenidaViajero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bienvenida_viajero);
+    }
+}
