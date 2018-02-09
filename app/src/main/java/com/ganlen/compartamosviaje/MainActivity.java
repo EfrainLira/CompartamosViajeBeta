008109e1515f6b7ac186ce4b6ddcbba5538fdee2
Fernando V.G <driber_rx8@hotmail.com>
2018-02-09 23:38:50 +0000

    feat: clear code

--- app/src/main/java/com/ganlen/compartamosviaje/MainActivity.java
+++ app/src/main/java/com/ganlen/compartamosviaje/MainActivity.java
@@ -26,23 +26,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PackageInfo info;
-        try {
-            info = getPackageManager().getPackageInfo("com.ganlen.compartamosviaje", PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                //String something = new String(Base64.encodeBytes(md.digest()));
-                Log.e("keyHash", something);
-            }
-        } catch (PackageManager.NameNotFoundException e1) {
-            Log.e("name not found", e1.toString());
-        } catch (NoSuchAlgorithmException e) {
-            Log.e("no such an algorithm", e.toString());
-        } catch (Exception e) {
-            Log.e("exception", e.toString());
-        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
